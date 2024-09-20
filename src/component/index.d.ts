581a7228e7a67a8c222581b7438e86856e5469a3
Evgenii Nikiforov <e@nikiforov.org>
2024-09-21 02:18:38 +0300

    try another ts

--- src/component/index.d.ts
+++ src/component/index.d.ts
@@ -1,11 +1,16 @@
+// index.d.ts
 declare module 'react-doc-render' {
+    import { FC } from 'react';
+
     export interface DocRenderProps {
         uri: string;
         config?: {
-            loading?: React.FC;
+            loading?: FC;
             renderers?: {
                 [key: string]: Function;
             };
         };
     }
+
+    export const DocRender: FC<DocRenderProps>;
 }
